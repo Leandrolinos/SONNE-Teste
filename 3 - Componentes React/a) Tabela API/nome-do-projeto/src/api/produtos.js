53b607a6c2905aa9b43f8eed8ba57485bf070fc8
Leandro Oliveira <leandro.alves.oliveira@gmail.com>
2023-04-16 21:28:47 -0300

    Exercicio 03

---
+++ 3 - Componentes React/a) Tabela API/nome-do-projeto/src/api/produtos.js
@@ -0,0 +1,9 @@
+const produtos = [
+    { id: 1, nome: 'Produto 1', categoria: 'Categoria 1' },
+    { id: 2, nome: 'Produto 2', categoria: 'Categoria 2' },
+    { id: 3, nome: 'Produto 3', categoria: 'Categoria 1' },
+    { id: 4, nome: 'Produto 4', categoria: 'Categoria 2' },
+    { id: 5, nome: 'Produto 5', categoria: 'Categoria 1' },
+];
+
+export default produtos;
