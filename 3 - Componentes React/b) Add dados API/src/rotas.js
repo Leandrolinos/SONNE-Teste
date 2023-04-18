a8ce8f0e6221f0ef45af8f7285d9ca68e4782ae3
Leandro Alves de Oliveira <114179738+Leandrolinos@users.noreply.github.com>
2023-04-17 21:41:20 -0300

    Delete 3 - Componentes React/b) Add dados API directory

--- 3 - Componentes React/b) Add dados API/src/rotas.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const managers = require('./controladores/managers')
-
-const rotas = express();
-
-rotas.get('/managers', managers.listManagers);
-rotas.post('/managers', managers.cadastroManagers);
-
-module.exports = rotas
